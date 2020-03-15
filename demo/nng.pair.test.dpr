6c0d3960f20270c43519309e6ce092b5caae153f
Pierre Yager <pierre.yager@crisalid.com>
2020-03-15 22:43:11 +0100

    Complexified a bit the message structure

--- demo/nng.pair.test.dpr
+++ demo/nng.pair.test.dpr
@@ -14,7 +14,10 @@ uses
 var
   S1, S2: Tnng_socket;
   M: Pnng_msg;
-  I: Integer;
+  I, N: Integer;
+  L: Word;
+  J: Cardinal;
+  S: PAnsiChar;
 begin
   try
     nng_pair_open(S1);
@@ -23,16 +26,31 @@ begin
     nng_listen(S1, 'tcp://localhost:5555', nil, 0);
     nng_dial(S2, 'tcp://localhost:5555', nil, Ord(NNG_FLAG_NONBLOCK));
 
-    for I := 0 to 10000 do
+    N := 0;
+    for I := 1 to 10000 do
     begin
-      Write('.');
       nng_msg_alloc(M, 0);
+
+      nng_msg_append_u16(M, 6);
       nng_msg_append(M, PAnsiChar('HELLO'), 6);
+      nng_msg_append_u32(M, I);
+
       nng_sendmsg(S1, M, 0);
       nng_recvmsg(S2, M, 0);
-      Assert(nng_msg_len(M) = Length('HELLO') + 1);
+
+      nng_msg_trim_u16(M, L);
+      Assert(L = 6);
+      S := nng_msg_body(M);
+      Assert(S = 'HELLO');
+      nng_msg_trim(M, L);
+      nng_msg_trim_u32(M, J);
+      Assert(J = Cardinal(I));
+
       nng_msg_free(M);
+      Inc(N);
     end;
+
+    WriteLn(Format('%d messages received', [N]));
 
     nng_close(S1);
     nng_close(S2);
